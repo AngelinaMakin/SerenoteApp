850723bcbf71bed0870194dff9ec5625f1020a6f
YuliAstiPutri <yuliastiputri@gmail.com>
2025-06-05 22:36:49 +0700

    Menambahkan konfigurasi NoteDatabase untuk Room

--- app/src/main/java/com/uti/app/NoteDatabase.kt
+++ app/src/main/java/com/uti/app/NoteDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.serenoteapp.model.Note
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
