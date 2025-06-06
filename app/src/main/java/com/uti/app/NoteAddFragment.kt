7e592b90450aab4aa96f6d07f54e5a20b94c0fb0
YuliAstiPutri <yuliastiputri@gmail.com>
2025-06-06 17:34:55 +0700

    Tambahkan ViewModel ke NoteAddFragment untuk akses data

--- app/src/main/java/com/uti/app/NoteAddFragment.kt
+++ app/src/main/java/com/uti/app/NoteAddFragment.kt
@@ -5,12 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.serenoteapp.data.NoteDatabase
+import com.example.serenoteapp.data.NoteRepository
 import com.example.serenoteapp.databinding.FragmentNoteAddBinding
+import com.example.serenoteapp.viewmodel.NoteViewModel
+import com.example.serenoteapp.viewmodel.NoteViewModelFactory
 
 class NoteAddFragment : Fragment() {
 
     private var _binding: FragmentNoteAddBinding? = null
     private val binding get() = _binding!!
+
+   // Ditambahkan: Inisialisasi ViewModel
+    private val noteViewModel: NoteViewModel by viewModels {
+        NoteViewModelFactory(
+            NoteRepository(NoteDatabase.getDatabase(requireContext()).noteDao())
+        )
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
