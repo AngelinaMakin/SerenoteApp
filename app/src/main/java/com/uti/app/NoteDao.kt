6a7c25f8c9d28cd86abfc84827c7bf63b8142ab4
AngelinaMakin <maria_angelina_luisa_makin@teknokrat.ac.id>
2025-06-12 19:07:33 +0700

    Update DAO untuk handle field waktuuu

--- app/src/main/java/com/uti/app/NoteDao.kt
+++ app/src/main/java/com/uti/app/NoteDao.kt
@@ -9,12 +9,12 @@ interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: Note)
 
-    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
-    fun getAllNotes(): Flow<List<Note>>  // Ubah jadi Flow
-
     @Update
     suspend fun updateNote(note: Note)
 
     @Delete
     suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    fun getAllNotes(): Flow<List<Note>>
 }
