36eec5e2891984930c4a2f013ab2d00cf78ab150
AngelinaMakin <Enjelmakin@gmail.com>
2025-06-12 22:17:43 +0700

    Update Note.kt

--- app/src/main/java/com/uti/app/Note.kt
+++ app/src/main/java/com/uti/app/Note.kt
@@ -6,17 +6,15 @@ import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+
 @Entity(tableName = "notes")
+
 data class Note(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
     val content: String,
     val timestamp: Long,
-
     val createdAt: Long = System.currentTimeMillis(),
     val updatedAt: Long = System.currentTimeMillis()
 ) : Parcelable
-
-
-
