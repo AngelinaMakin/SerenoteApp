d9a98277188e3f049dc7a357a04297909f9f30fc
AngelinaMakin <Enjelmakin@gmail.com>
2025-06-14 01:03:00 +0700

    menambahkan fungsi deleteAllNotes() ke dalam NoteRepository

--- app/src/main/java/com/uti/app/NoteRepository.kt
+++ app/src/main/java/com/uti/app/NoteRepository.kt
@@ -7,7 +7,7 @@ class NoteRepository(private val noteDao: NoteDao) : INoteRepository {
     override suspend fun insertNote(note: Note) {
         noteDao.insertNote(note)
     }
-//Pastikan getAllNotes di repository mengembalikan Flow
+
     override fun getAllNotes(): Flow<List<Note>> = noteDao.getAllNotes()
 
     override suspend fun updateNote(note: Note) {
@@ -16,5 +16,9 @@ class NoteRepository(private val noteDao: NoteDao) : INoteRepository {
 
     override suspend fun deleteNote(note: Note) {
         noteDao.deleteNote(note)
+    }
+
+    override suspend fun deleteAllNotes() {
+        noteDao.deleteAllNotes()
     }
 }
